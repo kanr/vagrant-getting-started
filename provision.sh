93295b8dee574a2e9dc80eee14e06d2d46fdc8c0
Connor <connor.aitken@gmail.com>
2018-03-09 14:51:42 -0800

    Created versioning for the nginx configuraiton.

--- provision.sh
+++ provision.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
-# Provisioning Script
+#Author Connor Aitken
+#Provisioning Script
 
 apt-get -y update
 
+#install nginx
 apt-get -y install nginx
 
-#Create Symbolic link for the nginx root directory
+#remove sites enabled folder and copy in configuration
+rm -rf /etc/nginx/sites-enabled
+cp -r /vagrant/sites-enabled /etc/nginx
+
+#Create Symbolic link for the nginx root directory to working files
 rm -rf /usr/share/nginx/www/
 ln -s /vagrant/www  /usr/share/nginx/www
 
