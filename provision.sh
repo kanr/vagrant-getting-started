0e9731d4c7e491617798065bf28de93b513ddb5c
Connor <connor.aitken@gmail.com>
2018-03-08 17:54:59 -0800

    adding .gitignore and cleaning up vagrant working files

--- provision.sh
+++ provision.sh
@@ -6,4 +6,8 @@ apt-get -y update
 
 apt-get -y install nginx
 
+#Create Symbolic link for the nginx root directory
+rm -rf /usr/share/nginx/www/
+ln -s /vagrant/www  /usr/share/nginx/www
+
 service nginx start
